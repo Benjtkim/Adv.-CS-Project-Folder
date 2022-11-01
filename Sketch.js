0949cd2003206209289b094829dcecea98a588f5
Ben Kim <ben.jt.kim@gmail.com>
2022-11-01 13:15:52 -0700

    first commit

---
+++ Sketch.js
@@ -0,0 +1,12 @@
+
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+    console.log('Hello World!');
+}
+  
+function draw() {
+  text('|\---/|\n| o_o |\n\_^_/', width/2, 50);
+  rect(width/2, 10, 100);
+    
+}
+
