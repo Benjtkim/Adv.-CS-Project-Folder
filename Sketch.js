4685056926d498baaeaaef4afc6daa057a481940
Ben Kim <ben.jt.kim@gmail.com>
2022-11-02 21:21:06 -0700

    third commit

--- Sketch.js
+++ Sketch.js
@@ -1,31 +1,29 @@
 let powerups = [];
+let pos = 0;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  powerups.push(new Powerup(10, 500));
-  console.log("hi");
+  // powerups.push(new Powerup(10, 500));
 }
   
 function draw() {
   background(color('white'));
-  temmie(0, 100);
+  textSize(10);
+  text('░░░░░░░░░░░░▀▄▄▄▄▄▄▄▄▄▀░░░░░░░░░░░░░░░░', width/2, pos);
   for (const powerup of powerups) {
     powerup.display();
   }
 }
 
-
-function temmie(x, y) {
-
-  push();
-  translate(x, y);
-  textSize(10);
-  text('░░░░░░░░░░▄▄░░░░░░░░░░░░░░░░░░░░░░░░░░░', width/2, 50);
-  text('░░░░░░░░░██▀█▄░░▄██▀░░░░▄██▄░░░░░░░░░░░', width/2, 70);
-  text('░░░░░░░░██▄▄▄████████▄▄█▀░▀█░░░░░░░░░░░', width/2, 90);
-  text('░░░░░░░▄██████████████████▄█░░░░░░░░░░░', width/2, 120);
-  text('░░░░░░▄██████████████████████░░░░░░░░░░', width/2, 140);
-  text('░░░░░▄████████▀░▀█████████████░░░░░░░░░', width/2, 160);
-  pop();
+function mouseWheel(event) {
+  pos += event.delta;
+  return pos;
 }
+
+// function drawTemmieNeck() {
+//   push();
+//   textSize(10);
+//   text('░░░░░░░░░░░░▀▄▄▄▄▄▄▄▄▄▀░░░░░░░░░░░░░░░░', width/2, pos);
+//   pop();
+// }
 
