3ffd4fc31feb9c2a6cb5cf148a3605f011045438
Ben Kim <ben.jt.kim@gmail.com>
2022-11-13 12:55:27 -0800

    7th Commit

--- Sketch.js
+++ Sketch.js
@@ -1,5 +1,6 @@
 let powerups = [];
-scrollMeasure = 0;
+scrollMeasure1 = 0;
+scrollMeasure2 = 0;
 
 function setup() {
   background(255);
@@ -8,22 +9,34 @@ function setup() {
   
 function draw() {
   drawTemmieNeck();
+  drawTemWords();
   for (const powerup of powerups) {
     powerup.display();
   }
 }
 
 function mouseWheel(event) {
-  scrollMeasure += event.delta;
+  scrollMeasure1 += event.delta;
+  scrollMeasure2 += event.delta;
+  console.log(scrollMeasure2);
 }
 
 function drawTemmieNeck() {
-  if (scrollMeasure > 12) {
+  if (scrollMeasure1 > 12) {
     let element = document.getElementById("temmieNeck");
-    for (let i = 0; i < 15; i++) {
+    for (let i = 0; i < 10; i++) {
       element.innerHTML += "<div>░░░░░░░░░░░░▀▄▄▄▄▄▄▄▄▄▀░░░░░░░░░░░░░░░░</div>";
     }
-    scrollMeasure = 0;
+    scrollMeasure1 = 0;
   }
 }
 
+function drawTemWords() {
+  let temWords = ['hOI', 'famus', 'Muns', 'bOI', 'hOIVS', 'bOI', 'eg', 'colleg']; 
+  displayWord = temWords[ceil(random(0, temWords.length))];
+  if (scrollMeasure2 > 30) {
+    textSize(30);
+    text(displayWord, random(0, width), scrollMeasure2);
+    scrollMeasure2 = 0;
+  }
+}
